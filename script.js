40e7a67ffb9209228c6df177fb312fe1ae57b6f8
ankit nishad <ankitnishad3333@gmail.com>
2025-12-03 10:07:50 +0530

    s

---
+++ script.js
@@ -0,0 +1,8 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "Rony Dada - Cricket Master"; 
+document.getElementById("subscriberCount").innerText = "17,723+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
